5db6d7a3e32640ca5d4315941cfee2e77feb72b5
Justin Duncan <justinguyduncan@hotmail.com>
2023-06-20 21:50:02 -0700

    added more seeds

--- app/seeds/friendships.py
+++ app/seeds/friendships.py
@@ -1,37 +1,66 @@
 from app.models import db, Friendship, environment, SCHEMA
 from sqlalchemy.sql import text
 
-#userid 1 - demo
-#userid 2 - aurora
-#userid 3 - justin
-#userid 4 - dmytro
+# User IDs
+demo_id = 1
+aurora_id = 2
+justin_id = 3
+dmytro_id = 4
+will_id = 5
+anthony_id = 6
 
 def seed_friendships():
-    #demo friends to aurora
-    friendship1 = Friendship(
-        user_id=1, friend_id=2)
-    #aurora friends to demo
-    friendship2 = Friendship(
-        user_id=2, friend_id=1)
-    #demo friend to justin
-    friendship3 = Friendship(
-        user_id=1, friend_id=3)
-    #justin friend to demo
-    friendship4 = Friendship(
-        user_id=3, friend_id=1)
-    #demo friend to dmytro
-    friendship5 = Friendship(
-        user_id=1, friend_id=4)
-    #dmytro friend to demo
-    friendship6 = Friendship(
-        user_id=4, friend_id=1)
 
-    db.session.add(friendship1)
-    db.session.add(friendship2)
-    db.session.add(friendship3)
-    db.session.add(friendship4)
-    db.session.add(friendship5)
-    db.session.add(friendship6)
+    # Friendships between demo and other users
+    friendship1 = Friendship(user_id=demo_id, friend_id=aurora_id)
+    friendship2 = Friendship(user_id=demo_id, friend_id=justin_id)
+    friendship3 = Friendship(user_id=demo_id, friend_id=dmytro_id)
+    friendship4 = Friendship(user_id=demo_id, friend_id=will_id)
+    friendship5 = Friendship(user_id=demo_id, friend_id=anthony_id)
+
+    # Friendships between aurora and other users
+    friendship6 = Friendship(user_id=aurora_id, friend_id=demo_id)
+    friendship7 = Friendship(user_id=aurora_id, friend_id=justin_id)
+    friendship8 = Friendship(user_id=aurora_id, friend_id=dmytro_id)
+    friendship9 = Friendship(user_id=aurora_id, friend_id=will_id)
+    friendship10 = Friendship(user_id=aurora_id, friend_id=anthony_id)
+
+    # Friendships between justin and other users
+    friendship11 = Friendship(user_id=justin_id, friend_id=demo_id)
+    friendship12 = Friendship(user_id=justin_id, friend_id=aurora_id)
+    friendship13 = Friendship(user_id=justin_id, friend_id=dmytro_id)
+    friendship14 = Friendship(user_id=justin_id, friend_id=will_id)
+    friendship15 = Friendship(user_id=justin_id, friend_id=anthony_id)
+
+    # Friendships between dmytro and other users
+    friendship16 = Friendship(user_id=dmytro_id, friend_id=demo_id)
+    friendship17 = Friendship(user_id=dmytro_id, friend_id=aurora_id)
+    friendship18 = Friendship(user_id=dmytro_id, friend_id=justin_id)
+    friendship19 = Friendship(user_id=dmytro_id, friend_id=will_id)
+    friendship20 = Friendship(user_id=dmytro_id, friend_id=anthony_id)
+
+    # Friendships between will and other users
+    friendship21 = Friendship(user_id=will_id, friend_id=demo_id)
+    friendship22 = Friendship(user_id=will_id, friend_id=aurora_id)
+    friendship23 = Friendship(user_id=will_id, friend_id=justin_id)
+    friendship24 = Friendship(user_id=will_id, friend_id=dmytro_id)
+    friendship25 = Friendship(user_id=will_id, friend_id=anthony_id)
+
+    # Friendships between anthony and other users
+    friendship26 = Friendship(user_id=anthony_id, friend_id=demo_id)
+    friendship27 = Friendship(user_id=anthony_id, friend_id=aurora_id)
+    friendship28 = Friendship(user_id=anthony_id, friend_id=justin_id)
+    friendship29 = Friendship(user_id=anthony_id, friend_id=dmytro_id)
+    friendship30 = Friendship(user_id=anthony_id, friend_id=will_id)
+
+    db.session.add_all([
+        friendship1, friendship2, friendship3, friendship4, friendship5,
+        friendship6, friendship7, friendship8, friendship9, friendship10,
+        friendship11, friendship12, friendship13, friendship14, friendship15,
+        friendship16, friendship17, friendship18, friendship19, friendship20,
+        friendship21, friendship22, friendship23, friendship24, friendship25,
+        friendship26, friendship27, friendship28, friendship29, friendship30
+    ])
     db.session.commit()
 
 def undo_friendships():
