bf04a1a61fd3081e3d7ac0903ab6315d736e199c
Aurora Ignacio <bellaignacio10@gmail.com>
2023-06-22 18:42:53 -0700

    added delete cascade from expenses to participants, comments

--- app/models/expense.py
+++ app/models/expense.py
@@ -15,9 +15,9 @@ class Expense(db.Model):
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    participants = db.relationship('ExpenseParticipant', back_populates='expense')
+    participants = db.relationship('ExpenseParticipant', back_populates='expense', cascade="all, delete-orphan")
     user = db.relationship('User', back_populates='expenses')
-    comments = db.relationship('Comment', back_populates='expense')
+    comments = db.relationship('Comment', back_populates='expense', cascade="all, delete-orphan")
 
     def to_dict(self):
         return {
