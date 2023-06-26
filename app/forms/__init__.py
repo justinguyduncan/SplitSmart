df8ec0e7a57158ce98ca2d437672844b49430100
Aurora Ignacio <bellaignacio10@gmail.com>
2023-06-25 23:13:00 -0700

    edited payment routes

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,4 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .expense_form import ExpenseForm
 from .comment_form import CommentForm
+from .payment_form import PaymentForm
