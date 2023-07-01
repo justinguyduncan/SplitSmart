56a58ea9cad4fce2a818d6798c85cadc1a47fe2c
Aurora Ignacio <bellaignacio10@gmail.com>
2023-07-01 15:30:05 -0700

    added screenshot app images

--- react-app/src/components/AboutPage/index.js
+++ react-app/src/components/AboutPage/index.js
@@ -1,4 +1,8 @@
 import './AboutPage.css';
+import trackBalances from './track-balances.png';
+import organizeExpenses from './organize-expenses.png';
+import addExpensesEasily from './add-expenses-easily.png';
+import payFriendsBack from './pay-friends-back.png';
 
 function AboutPage() {
     return (
@@ -6,21 +10,11 @@ function AboutPage() {
             <div className="slideshow">
                 <h1>Less stress when sharing expenses<span id="swap-text"> </span></h1>
             </div>
-            <div className="track-balances">
-                <h2>Track balances</h2>
-                <h3>Keep track of shared expenses, balances, and who owes who.</h3>
-            </div>
-            <div className="organize-expenses">
-                <h2>Organize expenses</h2>
-                <h3>Split expenses with any group: trips, housemates, friends, and family.</h3>
-            </div>
-            <div className="add-expenses-easily">
-                <h2>Add expenses easily</h2>
-                <h3>Quickly add expenses on the go before you forget who paid.</h3>
-            </div>
-            <div className="pay-friends-back">
-                <h2>Pay friends back</h2>
-                <h3>Settle up with a friend and record any cash or online payment.</h3>
+            <div className="about-page-images">
+                <img className="track-balances" src={trackBalances}></img>
+                <img className="organize-expenses" src={organizeExpenses}></img>
+                <img className="add-expenses-easily" src={addExpensesEasily}></img>
+                <img className="pay-friends-back" src={payFriendsBack}></img>
             </div>
         </div>
     );
