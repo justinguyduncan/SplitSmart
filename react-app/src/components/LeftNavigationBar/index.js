c7c680dc6c55d49e85de5879cf8b2859aa602051
Aurora Ignacio <bellaignacio10@gmail.com>
2023-07-01 12:16:51 -0700

    finished LeftNavigation

--- react-app/src/components/LeftNavigationBar/index.js
+++ react-app/src/components/LeftNavigationBar/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import * as friendActions from '../../store/friend';
+import './LeftNavigation.css';
+import logo from './splitsmart-logo.png';
 
 function LeftNavigationBar() {
+    const dispatch = useDispatch();
+    const activeFriends = useSelector(state => Object.values(state.friend.friendships));
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        dispatch(friendActions.fetchFriendships())
+            .then(() => setIsLoaded(true));
+    }, [dispatch]);
+
     return (
         <div className="left-nav-bar">
-            <h1>LeftNavigation Bar</h1>
-            <NavLink to="/dashboard"><i className="fas fa-solid fa-comments-dollar" />Dashboard</NavLink>
+            <NavLink to="/dashboard"><img className="dashboard-logo" src={logo}></img>Dashboard</NavLink>
             <NavLink to="/all"><i className="fas fa-solid fa-list" />All Expenses</NavLink>
-            <div className="friends-list">
-
-            </div>
+            <p>FRIENDS</p>
+            {isLoaded && activeFriends?.map(friendObj => (
+                <NavLink to={`/friends/${friendObj.id}`}><i className="fas fa-solid fa-user" />{friendObj.friend.name}</NavLink>
+            ))}
         </div>
     );
 }
