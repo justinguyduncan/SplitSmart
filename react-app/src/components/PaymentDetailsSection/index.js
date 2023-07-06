d9581a1ff9a1ef21a8bf83ff428734bcf4af1926
Dmytro-Yakovenko <d.yakovenko1986@gmail.com>
2023-07-05 20:21:25 -0700

    payments

--- react-app/src/components/PaymentDetailsSection/index.js
+++ react-app/src/components/PaymentDetailsSection/index.js
@@ -1,31 +1,81 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchReceivedPayments, fetchSentPayments } from "../../store/payment";
+import { fetchFriendshipById } from "../../store/friend";
+import './PaymentDetailsSection.css'
 
+const month= ["January","February","March","April","May","June","July",
+            "August","September","October","November","December"];
 function PaymentDetailsSection({ paymentId }) {
   const dispatch = useDispatch();
-
   const payments = useSelector((state)=>state.payment)
-//   const sentPayments = useSelector((state)=>state.payment.sentPayments)
   const allPayments ={...payments?.receivedPayments, ...payments?.sentPayments}
   const payment =allPayments[paymentId]
-  console.log(payment)
+  const date= new Date(payment?.created_at)
+  const createdDate = `${month[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()} `
+  const selectedFriendship = useSelector((state)=>state.friend?.selectedFriendship)
+ const user = selectedFriendship?.user
+ const friend =selectedFriendship?.friend
+
+
+
+
   useEffect(() => {
     dispatch(fetchReceivedPayments());
     dispatch(fetchSentPayments());
   }, [dispatch]);
+
+console.log(selectedFriendship?.user?.short_name, 3333333)
+
+  useEffect(()=>{
+    if(payment?.friendship_id){
+        dispatch(fetchFriendshipById(payment?.friendship_id))
+    }
+  
+  },[dispatch, payment?.friendship_id])
   return (
     <>
       <h1>PaymentDetails Section</h1>
       <section className="subheader">
+        <div className="image-wrapper">
         <img
           src="https://assets.splitwise.com/assets/api/payment_icon/square/large/offline.png"
           alt="dollar sighn"
         />
+        
+        </div>
+       
         <p>Payment</p>
-      </section>
+        <p>${+payment?.amount}.00</p>
+        <p>Added by {selectedFriendship?.user?.short_name} on {createdDate}</p>
 
-      <section className="main-content"></section>
+        <button className="edit-btn" onClick={()=>alert("feature coming soon")}>
+            Edit payment
+        </button>
+
+
+      </section>
+      <hr/>
+
+      <section className="main-content">
+    <div className="main-content-wrapper">
+    <img src={friend?.image_url} alt="photo of friend"/>
+    <p>
+        {friend?.short_name} paid ${+payment?.amount}.00
+    </p>
+    </div>
+
+   
+
+    <div className="main-content-wrapper">
+    <img src={user?.image_url} alt="photo of user"/>
+    <p>
+        {user?.short_name} recieved ${+payment?.amount}.00
+    </p>
+        
+        </div>
+
+      </section>
     </>
   );
 }
