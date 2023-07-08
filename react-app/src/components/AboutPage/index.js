185db6c288e98488651ae34c7abee3be6270bdbf
Aurora Ignacio <bellaignacio10@gmail.com>
2023-07-08 13:07:19 -0700

    styled about page

--- react-app/src/components/AboutPage/index.js
+++ react-app/src/components/AboutPage/index.js
@@ -1,3 +1,6 @@
+import { useHistory } from "react-router";
+import { useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
 import './AboutPage.css';
 import trackBalances from './track-balances.png';
 import organizeExpenses from './organize-expenses.png';
@@ -5,10 +8,19 @@ import addExpensesEasily from './add-expenses-easily.png';
 import payFriendsBack from './pay-friends-back.png';
 
 function AboutPage() {
+    const history = useHistory();
+    const sessionUser = useSelector((state) => state.session.user);
+
+    if (sessionUser) return <Redirect to="/dashboard" />;
+
     return (
         <div className="about-page-container">
             <div className="slideshow">
                 <h1>Less stress when sharing expenses<span id="swap-text"> </span></h1>
+                <h3>Keep track of your shared expenses and balances with housemates, trips, groups, friends, and family.</h3>
+                <button className="signup-button" onClick={() => {
+                    history.push('/signup');
+                }}>Sign up</button>
             </div>
             <div className="about-page-images">
                 <img className="track-balances" src={trackBalances}></img>
