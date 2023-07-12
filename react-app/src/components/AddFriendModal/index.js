55712e9773fa34a2007438c27cea4f31e6344850
Aurora Ignacio <bellaignacio10@gmail.com>
2023-07-12 00:06:47 -0700

    set structure of Add Friend form

--- react-app/src/components/AddFriendModal/index.js
+++ react-app/src/components/AddFriendModal/index.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useModal } from '../../context/Modal';
+import logo from './splitsmart-logo.png';
 import './AddFriend.css'
 
 function AddFriendModal() {
@@ -10,7 +11,16 @@ function AddFriendModal() {
 
     return (
         <>
-            <h1>AddFriend Modal</h1>
+            <h1><img className="add-friend-logo" src={logo} alt="add-friend-logo"></img>Invite friends</h1>
+            <form>
+                <input
+                    placeholder="Enter email address"
+                ></input>
+                <input
+                    placeholder="Include an optional message"
+                ></input>
+                <button className="accent">Send invites and add friends</button>
+            </form>
         </>
     );
 }
