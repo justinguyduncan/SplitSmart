7ea1249a8edca8b9af5116b6c6c8cb2c5deea826
Aurora Ignacio <bellaignacio10@gmail.com>
2023-07-13 19:36:24 -0700

    fixed orange/green formatting bugs

--- react-app/src/components/DashboardPage/index.js
+++ react-app/src/components/DashboardPage/index.js
@@ -6,6 +6,7 @@ import { getSummary } from "../../store/expense";
 import { fetchFriendships } from "../../store/friend";
 import TopNavigationBar from "../TopNavigationBar";
 import MainHeader from "../MainHeader";
+import settled from './all-settled-up.png';
 import "./DashboardPage.css";
 
 function DashboardPage() {
@@ -47,114 +48,128 @@ function DashboardPage() {
   if (!sessionUser) return <Redirect to="/" />;
 
   //render
-  return (
-    <>
-      <LeftNavigationBar />
-      <TopNavigationBar />
-      <MainHeader />
-      <main className="dashboard-main">
-        <section className="dashboard-subheader">
-          <ul className="dashboard-subheader-list">
-            <li>
-              <p className="dashboard-subheader-list-text">total balance </p>
-              {summary["you owe"] > summary["you are owed"] ? (
-                <p
-                  className={`dashboard-subheader-text dashboard-subheader-text-orange`}
-                >
-                  -{formatMoney(summary?.balance)}
-                </p>
-              ) : (
-                <p
-                  className={`dashboard-subheader-text ${
-                    summary?.balance
+
+  if (Number(summary?.balance) == 0) {
+    return (
+      <>
+        <LeftNavigationBar />
+        <TopNavigationBar />
+        <MainHeader />
+        <div className="dashboard-main">
+          <img src={settled} className="dashboard-settled-img" alt="all-settled-up" />
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <LeftNavigationBar />
+        <TopNavigationBar />
+        <MainHeader />
+        <main className="dashboard-main">
+          <section className="dashboard-subheader">
+            <ul className="dashboard-subheader-list">
+              <li>
+                <p className="dashboard-subheader-list-text">total balance </p>
+
+                {Number(summary["you owe"]) > Number(summary["you are owed"]) ? (
+                  <p
+                    className={`dashboard-subheader-text dashboard-subheader-text-orange`}
+                  >
+                    -{formatMoney(summary?.balance)}
+                  </p>
+                ) : (
+                  <p
+                    className={`dashboard-subheader-text ${summary?.balance
                       ? "dashboard-subheader-text-green"
                       : "dashboard-subheader-text-grey"
-                  }`}
-                >
-                  +{formatMoney(summary?.balance)}
-                </p>
-              )}
-            </li>
-            <li className="dashboard-subheader-item">
-              <p className="dashboard-subheader-list-text">you owe </p>
+                      }`}
+                  >
+                    +{formatMoney(summary?.balance)}
+                  </p>
 
-              <p
-                className={`dashboard-subheader-text ${
-                  summary["you owe"]
+
+                )}
+              </li>
+              <li className="dashboard-subheader-item">
+                <p className="dashboard-subheader-list-text">you owe </p>
+
+                <p
+                  className={`dashboard-subheader-text ${summary["you owe"]
                     ? "dashboard-subheader-text-orange"
                     : "dashboard-subheader-text-grey"
-                }`}
-              >
-                {formatMoney(summary["you owe"])}
-              </p>
-            </li>
-            <li>
-              <p className="dashboard-subheader-list-text">you are owed </p>
+                    }`}
+                >
+                  {formatMoney(summary["you owe"])}
+                </p>
+              </li>
+              <li>
+                <p className="dashboard-subheader-list-text">you are owed </p>
 
-              <p
-                className={`dashboard-subheader-text ${
-                  summary["you are owed"]
+                <p
+                  className={`dashboard-subheader-text ${summary["you are owed"]
                     ? "dashboard-subheader-text-green"
                     : "dashboard-subheader-text-grey"
-                }`}
-              >
-                {formatMoney(summary["you are owed"])}
-              </p>
-            </li>
-          </ul>
-        </section>
+                    }`}
+                >
+                  {formatMoney(summary["you are owed"])}
+                </p>
+              </li>
+            </ul>
+          </section>
 
-        <section className="dashboard-owed-section">
-          <div className="dashboard-owed-wrapper">
-            <h4 className="dashboard-owed-title">YOU OWE</h4>
-            {youOwe.length ? (
-              <ul className="dashboard-owed-list">
-                {youOwe.map((item) => (
-                  <li className="dashboard-owed-item" key={item.id}>
-                    <NavLink to={`/friends/${item.id}`}>
-                      <img src={item.friend.image_url} alt={item.friend.name} />
-                      <div>
-                        <p>{item.friend.name}</p>
-                        <p className="dashboard-subheader-text-orange">
-                          {formatMoney(item.bill)}
-                        </p>
-                      </div>
-                    </NavLink>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p className="dashboard-no-owe">You don't owe anything</p>
-            )}
-          </div>
-          <div className="dashboard-owed-wrapper dashboard-owed-wrapper-right">
-            <h4 className="dashboard-owed-title dashboard-owed-title-right ">
-              YOU ARE OWED
-            </h4>
-            {youAreOwed.length ? (
-              <ul className="dashboard-owed-list">
-                {youAreOwed.map((item) => (
-                  <li className="dashboard-owed-item" key={item.id}>
-                    <NavLink to={`/friends/${item.id}`}>
-                      <img src={item.friend.image_url} alt={item.friend.name} />
-                      <div>
-                        <p>{item.friend.name}</p>
-                        <p className="dashboard-subheader-text-green">
-                          owes you {formatMoney(item.bill)}
-                        </p>
-                      </div>
-                    </NavLink>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p className="dashboard-no-owe">You are not owed anything</p>
-            )}
-          </div>
-        </section>
-      </main>
-    </>
-  );
+          <section className="dashboard-owed-section">
+            <div className="dashboard-owed-wrapper">
+              <h4 className="dashboard-owed-title">YOU OWE</h4>
+              {youOwe.length !== 0 ? (
+                <ul className="dashboard-owed-list">
+                  {youOwe.map((item) => (
+                    <li className="dashboard-owed-item" key={item.id}>
+                      <NavLink to={`/friends/${item.id}`}>
+                        <img src={item.friend.image_url} alt={item.friend.name} />
+                        <div>
+                          <p>{item.friend.name}</p>
+                          <p className="dashboard-subheader-text-orange">
+                            you owe {formatMoney(item.bill)}
+                          </p>
+                        </div>
+                      </NavLink>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p className="dashboard-no-owe">You don't owe anything</p>
+              )}
+            </div>
+            <div className="dashboard-owed-wrapper dashboard-owed-wrapper-right">
+              <h4 className="dashboard-owed-title dashboard-owed-title-right ">
+                YOU ARE OWED
+              </h4>
+              {youAreOwed.length !== 0 ? (
+                <ul className="dashboard-owed-list">
+                  {youAreOwed.map((item) => (
+                    <li className="dashboard-owed-item" key={item.id}>
+                      <NavLink to={`/friends/${item.id}`}>
+                        <img src={item.friend.image_url} alt={item.friend.name} />
+                        <div>
+                          <p>{item.friend.name}</p>
+                          <p className="dashboard-subheader-text-green">
+                            owes you {formatMoney(item.bill)}
+                          </p>
+                        </div>
+                      </NavLink>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p className="dashboard-no-owe dashboard-no-owe-right">You are not owed anything</p>
+              )}
+            </div>
+          </section>
+        </main>
+      </>
+    );
+  }
 }
 
 export default DashboardPage;
